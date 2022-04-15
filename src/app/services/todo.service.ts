e4b6827dc4e7b2e19bca0d8842fa7d3a212d547d
--global <--global>
2022-04-15 15:55:54 +0500

    added the CRUD operation method

--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -1,13 +1,32 @@
-import { Injectable } from '@angular/core';
-
-
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { Todo } from "../_models/todo";
 
 export const SERVER_URL: string = "/api";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class TodoService {
+  apiURL = "https://jsonplaceholder.typicode.com/todos";
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getTodo(): Observable<Todo[]> {
+    return this.http.get<Todo[]>(this.apiURL).pipe();
+  }
+
+  saveNewTodo(data: Todo): Observable<Todo> {
+    return this.http.post<Todo>(this.apiURL, data);
+  }
+
+  updateTodo(todo: any) {
+    todo.title = "I am going to update this";
+    return this.http.put(this.apiURL + "/" + todo.id, todo);
+  }
+
+  deleteTodo(id: number): Observable<{}> {
+    return this.http.delete<{}>(this.apiURL + "/" + id);
+  }
 }
